923d01bec3cec46abb85d4c461f2d3f0287351cd
Ducthien <ducthien1824@gmail.com>
2026-03-13 17:04:24 +0700

    update file vi en

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -1122,44 +1122,4 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String get zone_exit_safe_child => 'ℹ️ You left a safe zone';
-
-  @override
-  String get tracking_location_service_off_parent_title =>
-      'Child turned off location';
-
-  @override
-  String get tracking_location_permission_denied_parent_title =>
-      'Child turned off location permission';
-
-  @override
-  String get tracking_background_disabled_parent_title =>
-      'Background location was turned off';
-
-  @override
-  String get tracking_location_stale_parent_title =>
-      'No recent location update';
-
-  @override
-  String get tracking_ok_parent_title => 'Location is active again';
-
-  @override
-  String get tracking_location_service_off_child_title =>
-      'Location is turned off';
-
-  @override
-  String get tracking_location_permission_denied_child_title =>
-      'Location permission is off';
-
-  @override
-  String get tracking_background_disabled_child_title =>
-      'Background location is off';
-
-  @override
-  String get tracking_location_stale_child_title => 'Location is not updating';
-
-  @override
-  String get tracking_ok_child_title => 'Location is working again';
-
-  @override
-  String get tracking_default_title => 'Tracking notification';
 }
